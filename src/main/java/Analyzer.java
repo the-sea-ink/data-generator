eb1982070430cf6b7f7da2eb10d35c09670bbd52
xenia-melman <xenia.melman@gmail.com>
2021-03-22 12:33:11 +0100

    first commit

---
+++ src/main/java/Analyzer.java
@@ -0,0 +1,12 @@
+public class Analyzer {
+    //for statistics
+
+    public static void main(String[] args) {
+
+    }
+    public void countOutOfOrder() {};
+    public void countTransactionsPerSecond() {};
+
+
+
+}
