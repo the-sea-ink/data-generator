e8e0ac1daede4d545aa9adee8600be8929d76975
xenia-melman <xenia.melman@gmail.com>
2021-03-22 14:10:35 +0100

    adding exporter functions

--- src/main/java/HelperClasses/Exporter.java
+++ src/main/java/HelperClasses/Exporter.java
@@ -1,5 +1,24 @@
 package HelperClasses;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Exporter {
+
+    public static void exporterInit() throws IOException {
+        FileWriter csvWriter = new FileWriter("output/output.csv");
+    }
+
+    public static String stringGenerator(int eventID, int sensorID, String eventTime, double waterTemperature){
+        //eventID, sensorID, eventTime, waterTemperature
+        String dataString = eventID + "," + sensorID + "," + eventTime + "," + waterTemperature + "\n";
+        return dataString;
+    }
+
+    public static void exporter(FileWriter csvWriter, String dataString) throws IOException {
+        csvWriter.append(dataString);
+        csvWriter.flush();
+        csvWriter.close();
+    }
 
 }
