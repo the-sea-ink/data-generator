974110f6289819e0e769dedbfb40e3a60a675f7d
xenia-melman <xenia.melman@gmail.com>
2021-03-22 14:10:35 +0100

    adding exporter functions

--- src/main/java/Generator.java
+++ src/main/java/Generator.java
@@ -1,12 +1,17 @@
+import HelperClasses.Exporter;
 import org.json.simple.parser.ParseException;
 
 import java.io.IOException;
 
 public class Generator {
 
-    public static void main(String[] args) throws IOException, ParseException {
+    public static void main(String[] args) throws IOException {
+        Exporter.exporterInit();
 
     }
+
+
+
 }
 
 
