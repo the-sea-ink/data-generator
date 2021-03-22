c27c52130a2861665be593ae22e9a584d90d7142
xenia-melman <xenia.melman@gmail.com>
2021-03-22 16:09:59 +0100

    exporter functions

--- src/main/java/HelperClasses/Exporter.java
+++ src/main/java/HelperClasses/Exporter.java
@@ -2,17 +2,28 @@ package HelperClasses;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.List;
 
 public class Exporter {
 
-    public static void exporterInit() throws IOException {
-        FileWriter csvWriter = new FileWriter("output/output.csv");
+    public static <String> List<String> listGenerator(List<String> list, String item) {
+        list.add(item);
+        return list;
     }
 
-    public static String stringGenerator(int eventID, int sensorID, String eventTime, double waterTemperature){
-        //eventID, sensorID, eventTime, waterTemperature
-        String dataString = eventID + "," + sensorID + "," + eventTime + "," + waterTemperature + "\n";
+    public static String stringGenerator(List<String>stringList){
+
+        String dataString = "";
+        for (String element : stringList) {
+            dataString += element + ",";
+        }
+        dataString += "\n";
         return dataString;
+    }
+
+    public static FileWriter exporterInit() throws IOException {
+        FileWriter csvWriter = new FileWriter("output/output.csv");
+        return csvWriter;
     }
 
     public static void exporter(FileWriter csvWriter, String dataString) throws IOException {
