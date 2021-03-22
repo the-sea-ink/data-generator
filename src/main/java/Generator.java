ed8829ad0c2907c456fcf6e54ffef2734560b3f5
xenia-melman <xenia.melman@gmail.com>
2021-03-22 16:09:59 +0100

    exporter functions

--- src/main/java/Generator.java
+++ src/main/java/Generator.java
@@ -1,12 +1,24 @@
-import HelperClasses.Exporter;
-import org.json.simple.parser.ParseException;
 
+import HelperClasses.Exporter;
+
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.List;
 
 public class Generator {
 
     public static void main(String[] args) throws IOException {
-        Exporter.exporterInit();
+        List<String> list = new ArrayList<>();
+
+        Exporter.listGenerator(list, "1");
+        Exporter.listGenerator(list, "2");
+
+        String string = Exporter.stringGenerator(list);
+
+        FileWriter fileWriter = Exporter.exporterInit();
+        Exporter.exporter(fileWriter, string);
 
     }
 
