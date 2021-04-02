7de36be482c2675805e2751a367aa44713ade055
xenia-melman <xenia.melman@gmail.com>
2021-04-02 15:36:17 +0200

    timestamps

--- src/main/java/HelperClasses/Exporter.java
+++ src/main/java/HelperClasses/Exporter.java
@@ -3,7 +3,6 @@ package HelperClasses;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.List;
 
 public class Exporter {
 
--- src/main/java/HelperClasses/TimeHandler.java
+++ src/main/java/HelperClasses/TimeHandler.java
@@ -1,18 +1,29 @@
 package HelperClasses;
+import org.json.simple.parser.ParseException;
+import java.io.IOException;
 import java.sql.Timestamp;
 import java.util.Date;
-import java.text.SimpleDateFormat;
-import java.util.concurrent.TimeUnit;
 
-import static jdk.nashorn.internal.runtime.regexp.joni.Syntax.Java;
 
 public class TimeHandler {
 
-    public static String timeConverter () {
-        Timestamp ts = new Timestamp(System.currentTimeMillis());
-        Date date=ts;
+    public static String timeConverter () throws IOException, ParseException, java.text.ParseException {
+        //Timestamp ts = new Timestamp(System.currentTimeMillis());
+        //Date date=ts;
+
+        Date date = ConfigReader.getStartingTime();
         System.out.println(date);
+
+        Date d = new Date(date.getTime() + 1000);
+        System.out.println(d);
+        Timestamp ts = new Timestamp(date.getTime());
+        System.out.println(ts);
         return "1223";
+    }
+
+    public static Date addTime (Date date, int time) {
+        Date newDate = new Date (date.getTime() + time);
+        return newDate;
     }
 
 }
