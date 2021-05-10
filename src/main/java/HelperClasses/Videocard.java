8772a96434dfca2c4618fb377ae4dfcee412bd35
xenia-melman <xenia.melman@gmail.com>
2021-05-10 14:33:43 +0200

    simple use case added

---
+++ src/main/java/HelperClasses/Videocard.java
@@ -0,0 +1,47 @@
+package HelperClasses;
+import java.util.Random;
+
+public class Videocard {
+
+    public int serialNumber;
+    public double temperature;
+    public boolean overheatWarning;
+    public int overheatWarningTemperature;
+
+    public Videocard() {
+        this.serialNumber = randomSerialNumberGenerator();
+        this.temperature = 40;
+        this.overheatWarning = false;
+        this.overheatWarningTemperature = 50;
+    }
+
+    public void Updater() {
+        temperatureUpdater();
+        overheatUpdater();
+    }
+
+    public int randomSerialNumberGenerator () {
+        Random randomNumber = new Random();
+        int random = randomNumber.nextInt(999999);
+        return random;
+    }
+
+    public int temperatureRandomizer () {
+        return (int) (Math.random() + 1.5 );
+    }
+
+    public void temperatureUpdater () {
+        if ((temperatureRandomizer()<2 || this.temperature<=30) && this.temperature < 70 ) {
+            this.temperature += temperatureRandomizer();
+        }else {
+            this.temperature -= temperatureRandomizer();
+        }
+    }
+
+    public void overheatUpdater () {
+        if (this.temperature >= 50)
+            overheatWarning = true;
+        else
+            overheatWarning = false;
+    }
+}
