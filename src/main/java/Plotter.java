5230b97125c88551ff685b8396301889ba3c36e2
xenia-melman <xenia.melman@gmail.com>
2021-05-13 15:02:15 +0200

    histogram added

--- src/main/java/Plotter.java
+++ src/main/java/Plotter.java
@@ -1,5 +1,139 @@
 import javax.swing.JFrame;
 
+import HelperClasses.ConfigReader;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.statistics.HistogramDataset;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+
+import org.jfree.chart.ChartUtils;
+import org.json.simple.parser.ParseException;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Timestamp;
+import java.util.Date;
+
+
 public class Plotter extends JFrame {
 
+
+
+
+    public static void main(String[] args) throws IOException, ParseException {
+        lineChart();
+        histogram();
+
     }
+
+    public static void lineChart () throws IOException {
+        XYSeries series1 = new XYSeries("Data");
+        series1.add(18, 530);
+        series1.add(20, 580);
+        series1.add(25, 740);
+        series1.add(30, 901);
+        series1.add(40, 1300);
+        series1.add(50, 2219);
+
+        XYSeriesCollection dataset = new XYSeriesCollection();
+        dataset.addSeries(series1);
+
+        JFreeChart chart = ChartFactory.createXYLineChart(
+                "Average salary per age",
+                "Age",
+                "Salary (€)",
+                dataset,
+                PlotOrientation.VERTICAL,
+                true,
+                true,
+                false
+        );
+
+
+        ChartUtils.saveChartAsPNG(new File("line_chart.png"), chart, 450, 400);
+    }
+
+    public static void histogram() throws IOException, ParseException {
+        int size = Analyzer.getTotalEvents();
+        double[] delays = new double[size-1];
+
+        //read output
+        BufferedReader br = new BufferedReader(new FileReader("output/output.csv"));
+
+        //init event/processing time columns/values
+        int eventTimeColumn = ConfigReader.getEventTimeColumn() - 1;
+        int processingTimeColumn = ConfigReader.getProcessingTimeColumn() - 1;
+
+        String line = br.readLine();
+        String[] lineArray = line.split(",");
+
+        String eventTimeString = lineArray[eventTimeColumn];
+        Date evenTimeDate = new Date(Long.parseLong(eventTimeString));
+
+        String processingTimeString = lineArray[processingTimeColumn];
+        Date processingTimeDate = new Date(Long.parseLong(processingTimeString));
+
+        int i = 0;
+
+        while ((line = br.readLine()) != null) {
+
+            lineArray = line.split(",");
+
+            eventTimeString = lineArray[eventTimeColumn];
+            evenTimeDate = new Date(Long.parseLong(eventTimeString));
+
+            processingTimeString = lineArray[processingTimeColumn];
+            processingTimeDate = new Date(Long.parseLong(processingTimeString));
+
+
+            Timestamp tsPT = new Timestamp(processingTimeDate.getTime());
+            Timestamp tsET = new Timestamp(evenTimeDate.getTime());
+            long diff = tsPT.getTime() - tsET.getTime();
+
+            delays[i] = (double) diff;
+            System.out.println(delays[i]);
+            i++;
+
+
+        }
+
+
+
+
+        double[] vals = {
+
+                0.71477137, 0.55749811, 0.50809619, 0.47027228, 0.25281568,
+                0.66633175, 0.50676332, 0.6007552, 0.56892904, 0.49553407,
+                0.61093935, 0.65057417, 0.40095626, 0.45969447, 0.51087888,
+                0.52894806, 0.49397198, 0.4267163, 0.54091298, 0.34545257,
+                0.58548892, 0.3137885, 0.63521146, 0.57541744, 0.59862265,
+                0.66261386, 0.56744017, 0.42548488, 0.40841345, 0.47393027,
+                0.60882106, 0.45961208, 0.43371424, 0.40876484, 0.64367337,
+                0.54092033, 0.34240811, 0.44048106, 0.48874236, 0.68300902,
+                0.33563968, 0.58328107, 0.58054283, 0.64710522, 0.37801285,
+                0.36748982, 0.44386445, 0.47245989, 0.297599, 0.50295541,
+                0.39785732, 0.51370486, 0.46650358, 0.5623638, 0.4446957,
+                0.52949791, 0.54611411, 0.41020067, 0.61644868, 0.47493691,
+                0.50611458, 0.42518211, 0.45467712, 0.52438467, 0.724529,
+                0.59749142, 0.45940223, 0.53099928, 0.65159718, 0.38038268,
+                0.51639554, 0.41847437, 0.46022878, 0.57326103, 0.44913632,
+                0.61043611, 0.42694949, 0.43997814, 0.58787928, 0.36252603,
+                0.50937634, 0.47444256, 0.57992527, 0.29381335, 0.50357977,
+                0.42469464, 0.53049697, 0.7163579, 0.39741694, 0.41980533,
+                0.68091159, 0.69330702, 0.50518926, 0.55884098, 0.48618324,
+                0.48469854, 0.55342267, 0.67159111, 0.62352006, 0.34773486};
+
+
+        HistogramDataset dataset = new HistogramDataset();
+        dataset.addSeries("key", delays, 50);
+
+        JFreeChart histogram = ChartFactory.createHistogram("Normal distribution",
+                "delays", "frequency", dataset);
+
+        ChartUtils.saveChartAsPNG(new File("histogram.png"), histogram, 450, 400);
+    }
+}
